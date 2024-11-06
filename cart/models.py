ab5d19adaecd0bbb70e7e98f50d401e7c528d27c
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-06 10:42:43 +0100

    Add CartItem model to cart app
    Introduced a new CartItem model to track products and their quantities in the cart. This new model includes a ForeignKey to the Product model and a default quantity of 1.

--- cart/models.py
+++ cart/models.py
@@ -4,3 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
